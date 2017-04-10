babd9f1d55270af60403580fac1c2301b80766e6
Ernst du Plooy <eduplooy@gmail.com>
2017-04-10 15:32:59 +0200

    V2

--- piio_init.sh
+++ piio_init.sh
@@ -2,5 +2,5 @@
 
 /usr/sbin/i2cset -y 1 0x20 0x00 0x00	#Set ports 1-8 to outputs
 /usr/sbin/i2cset -y 1 0x20 0x01 0xE0    #Set ports 9-13 to outputs and 14,15,16 to inputs (1110 0000)
-/tools/piio/piio_write.py -a 0x20 -f 0	#Switch all ports off
+/home/piio/piio_write.py -a 0x20 -f 0	#Switch all ports off
 echo "Ports Initialised"
